10a817ab549dac64516eba9ac1a435f28e6634de
CyrineOue <73118357+CyrineOue@users.noreply.github.com>
2022-05-01 21:19:43 +0000

    Add files via upload

--- src/app/contracts/contracts.component.ts
+++ src/app/contracts/contracts.component.ts
@@ -1,25 +1,102 @@
 import { Component, OnInit } from '@angular/core';
-import { Contracts } from '../contracts';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Contract } from 'src/app/model/Contract';
+import { ContractService } from 'src/app/shared/contract/contract.service';
 
 @Component({
   selector: 'app-contracts',
   templateUrl: './contracts.component.html',
-  styleUrls: ['./contracts.component.scss']
+  //styleUrls: ['./contracts.component.scss']
 })
 export class ContractsComponent implements OnInit {
-  listContracts : any;
-  form : boolean = false;
-  contract !:Contracts;
-  closeResult! : string;
+  [x: string]: any;
 
+  listContracts : Contract [];
+  form : boolean = false;
+  closeResult! : string;
+  contract!: Contract;
   constructor(private contractService : ContractService, private modalService :NgbModal) { }
 
   ngOnInit(): void {
-    this.getAllContracts();;
+    
+    this.getContracts();
     this.contract ={
-      IDContract : null;
+      IDContract : null,
+      CreationDate : null, 
+      StartDate : null, 
+      EndDate : null, 
+      NetPremium : null,
+      TTCPremium: null, 
+      CeillingAmount: null, 
+      InstallmentsNB: null, 
+      Costatus: null,
+      Method: null, 
+      IDClientP: null, 
+      IDClientC: null,
+     
     }
 
   }
+
+  getContracts() {
+    this.contractService.getContracts().subscribe(res => {console.log(res); this.listContracts= res});
+  }
+
+  addContract(contract : Contract){
+    this.contractService.addContract(contract).subscribe(() => { this.getContracts(); this.form=false;});
+  }
+  editContract(contract: Contract){
+    this.contractService.editContract(contract).subscribe();
+  }
+
+
+  deleteContract(idContract : any){
+    this.contractService.deleteContract(idContract).subscribe(() => this.getContracts())
+  }
+  chargeCreditCard() {
+    let form = document.getElementsByTagName("form")[0];
+    (<any>window).Stripe.card.createToken({
+      number: form.cardNumber.value,
+      exp_month: form.expMonth.value,
+      exp_year: form.expYear.value,
+      cvc: form.cvc.value
+    }, (status: number, response: any) => {
+      if (status === 200) {
+        let token = response.id;
+        this.chargeCard(token);
+      } else {
+        console.log(response.error.message);
+      }
+    });
+  }
+  chargeCard(token: string) {
+    const headers = new Headers({'token': token });
+    this.httpClient.post('http://localhost:8087/MITMVC/APIPayment/charge', {}, {headers: headers}) .subscribe(resp => {
+        console.log(resp);
+      })
+  }
+
+  open(content: any): void {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+    }
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return  `with: ${reason}`;
+      }
+    }
+    closeForm(){
+  
+    }
+    cancel(){
+      this.form = false;
+    }
 
 }
