4d39917c242f7e1174a9b443eb6ba612053cf8ef
Gorgi-Mahmoud <78954959+Gorgi-Mahmoud@users.noreply.github.com>
2022-05-02 17:43:08 +0100

    Add files via upload

---
+++ shared/product.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/model/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  productURL = "api/mit/product/retrieve-all-products";
+  constructor(private http : HttpClient) { }
+  
+//Crud
+
+  getProducts() : Observable<Product[]>{
+    return this.http.get<Product[]>(this.productURL);
+  }
+
+  deleteProductById(id:number){
+    return this.http.delete<Product>("api/mit/product/remove-product/"+id);
+  }
+
+  editProduct(product: Product){
+    return this.http.put<Product>("api/mit/product/update-product",product);
+  }
+
+  addProduct(product:Product){
+    return this.http.post<Product>("api/mit/product/add-product",product);
+    }
+//Simulation
+
+  simulAHI(fieldvalue:number,location:string,fieldsize:number){
+    return this.http.get<number>("api/mit/product/ahipremuimcalculation/"+fieldvalue+"/"+location+"/"+fieldsize)
+  }
+
+  simulLSP(livestocknumber:number,livestockvalue:number,typels:string){
+    return this.http.get<number>("api/mit/product/almipremuimcalculation/"+livestocknumber+"/"+livestockvalue+"/"+typels)
+  }
+
+  simulSBFI(propertyvalue:number,sizeproperty:number){
+    return this.http.get<number>("api/mit/product/sbfipremuimcalculation/"+propertyvalue+"/"+sizeproperty)
+  }
+
+  simulBM(propertyvalue:number,insuredage:number){
+    return this.http.get<number>("api/mit/product/bmipremuimcalculation/"+propertyvalue+"/"+insuredage)
+  }
+  //SMS
+
+  SendSMS(productid:number){
+    return this.http.post("api/mit/api/v1/sms/"+productid,null);
+
+  }
+
+
+
+}
