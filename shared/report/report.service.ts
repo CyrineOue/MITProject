2621e1707346569767809d02b531b252a2076ef5
samar-jabri <73348338+samar-jabri@users.noreply.github.com>
2022-05-04 04:49:11 +0100

    shared

--- shared/report/report.service.ts
+++ shared/report/report.service.ts
@@ -25,4 +25,25 @@ export class ReportService {
   addReport(report:Report){
     return this.http.post<Report>("http://localhost:8081/mit/report/add-report",report);
     }
+
+  exportPdfReport(id:number): Observable<Blob>{
+    return this.http.get("http://localhost:8081/mit/pdf/generateReport/"+id, {responseType: 'blob'});
+  }
+
+  getTurnover() : Observable<number[]>{
+    return this.http.get<number[]>("http://localhost:8081/mit/report/retrieve-turnover");
+  }
+
+  getStartDate(from:Date,to:Date) : Observable<Report[]>{
+    return this.http.get<Report[]>("http://localhost:8081/mit/report/retrieve-report-ByDate/"+from+"/"+to);
+  }
+
+  searhhdata(model : any){  
+    debugger;    
+   return this.http.post<any>('http://localhost:1141/Api/Searchdata/search',model);    
+  }  
+  showdata(){  
+    debugger;    
+   return this.http.get<any>('http://localhost:1141/Api/Searchdata/showdata');    
+  }  
 }
