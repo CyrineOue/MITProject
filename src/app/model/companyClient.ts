dadc35ac816d30c517f66c2640b4c2f640189574
AyaHassine <73943315+ayahassine19@users.noreply.github.com>
2022-05-04 16:00:54 +0100

    Add files via upload

---
+++ src/app/model/companyClient.ts
@@ -0,0 +1,16 @@
+import { User } from "./user";
+
+export class CompanyClient {
+    idClientC : number;
+    brand:string;
+    activityArea: string ;
+    creationDate: Date ;
+    employeesNb: number ;
+    capital: number ;
+    nbDeclaredSinistersC: number ;
+    archived: boolean ;
+    sbuscriptionDate: Date ;
+    categoryC: string ;
+    logClientC:User;
+    ScoreC :number;
+}
