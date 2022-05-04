fc4b98b7c9281d1e1e750cf61a69d23646b75581
AyaHassine <73943315+ayahassine19@users.noreply.github.com>
2022-05-04 15:57:39 +0100

    Add files via upload

---
+++ src/app/shared/CompanyClient/company-client.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CompanyClient } from 'src/app/model/companyClient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyClientService {
+  CompanyClientUrl = "http://localhost:8081/mit/companyclient";
+  constructor(private http : HttpClient) { }
+  getCompanyClients() : Observable<CompanyClient[]>{
+    return this.http.get<CompanyClient[]>(this.CompanyClientUrl+"/retrieve-all-companyclients");
+  }
+
+  deleteCompanyClient(id:number){
+
+    return this.http.delete(`${this.CompanyClientUrl}/archive-companyclient/${id}`);
+    }
+
+  editCompanyClient(companyClient: CompanyClient){
+    return this.http.put<CompanyClient>(this.CompanyClientUrl+"/modify-companyclient/",companyClient);
+  }
+
+  addCompanyClient(companyClient:CompanyClient){
+    return this.http.post<CompanyClient>(this.CompanyClientUrl+"/add-companyclient",companyClient);
+    }
+  getScoreC(id :number){
+      return this.http.get(`${this.CompanyClientUrl}/score-companyClient/${id}`)
+    }
+  getCategoryC(id :number){
+    return this.http.get(`${this.CompanyClientUrl}/categorise-companyClient/${id}`)
+  }
+  getIdealClientC() {
+    return this.http.get(`${this.CompanyClientUrl}/GetIdealCompanyClient`);
+  }
+}
