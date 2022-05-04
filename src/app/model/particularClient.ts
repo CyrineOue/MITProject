0afbc5d0a5a434b8c8359ffd58d7dd9bdab22fd9
AyaHassine <73943315+ayahassine19@users.noreply.github.com>
2022-05-04 16:01:15 +0100

    Add files via upload

---
+++ src/app/model/particularClient.ts
@@ -0,0 +1,20 @@
+import { User } from "./user";
+
+export class ParticularClient {
+    idClientP :number;
+    firstName:string;
+    lastName:string;
+    profession:string;
+    cin:number;
+    birthDate:Date;
+    gender:string;
+    homeAddress:string;
+    phoneNo:number;
+    nbDeclaredSinistersP:number=0;
+    archived=false;
+    sbuscriptionDate :Date;
+    CategoryP:string;
+    logClientP:User;
+    ScoreP :number;
+
+}
